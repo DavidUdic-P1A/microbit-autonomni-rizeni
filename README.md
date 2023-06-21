258087774207bef7645c886c1d7f057f3ec79044
DavidUdic-P1A <115144133+DavidUdic-P1A@users.noreply.github.com>
2023-06-21 10:15:38 +0200

    Počáteční soubory projektu MakeCode

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Otevřít tuto stránku v aplikaci [https://davidudic-p1a.github.io/microbit-autonomni-rizeni/](https://davidudic-p1a.github.io/microbit-autonomni-rizeni/)
+
+## Použít jako rozšíření
+
+Toto úložiště lze přidat jako **rozšíření** v aplikaci MakeCode.
+
+* otevřít [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* klikněte na možnost **Nový projekt**
+* klikněte na možnost **Rozšíření** v nabídce s ozubeným kolem
+* vyhledat **https://github.com/davidudic-p1a/microbit-autonomni-rizeni** a importovat
+
+## Upravit tento projekt
+
+Slouží k úpravě tohoto úložiště v aplikaci MakeCode.
+
+* otevřít [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* klikněte na možnost **Import** a poté na **Import adresy URL**
+* vložte **https://github.com/davidudic-p1a/microbit-autonomni-rizeni** a klikněte na možnost import
+
+#### Metadata (slouží k vyhledávání, vykreslování)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,113 @@
+let motor1 = PCAmotor.Motors.M1
+let motor4 = PCAmotor.Motors.M4
+radio.setGroup(69)
+let leftSpeed = 60
+let rightSpeed = 60
+
+let direction = "0"
+let autonomous = true
+let whiteLine = 0
+
+const pinCenter = DigitalPin.P15
+const pinLeft = DigitalPin.P14
+const pinRight = DigitalPin.P13
+
+pins.setPull(pinCenter, PinPullMode.PullNone)
+
+basic.forever(function () {
+    if (autonomous) {
+        let centerSensor = (whiteLine ^ pins.digitalReadPin(pinCenter)) == 0 ? true : false
+        let leftSensor = (whiteLine ^ pins.digitalReadPin(pinLeft)) == 0 ? true : false
+        let rightSensor = (whiteLine ^ pins.digitalReadPin(pinRight)) == 0 ? true : false
+
+        if (!rightSensor && !leftSensor && direction == "r") {
+            basic.showIcon(IconNames.Duck, 0)
+            leftSpeed = -105
+            rightSpeed = -105
+            PCAmotor.MotorRun(motor1, leftSpeed)
+            PCAmotor.MotorRun(motor4, -rightSpeed)
+            basic.pause(600)
+            do {
+                rightSensor = (whiteLine ^ pins.digitalReadPin(pinRight)) == 0 ? true : false
+            } while (rightSensor)
+            basic.clearScreen()
+            direction = "0"
+        }
+
+        if (!rightSensor && !leftSensor && direction == "l") {
+            basic.showIcon(IconNames.Duck, 0)
+            leftSpeed = 105
+            rightSpeed = 115
+            basic.pause(100)
+            PCAmotor.MotorRun(motor1, leftSpeed)
+            PCAmotor.MotorRun(motor4, -rightSpeed)
+            basic.pause(600)
+            do {
+                rightSensor = (whiteLine ^ pins.digitalReadPin(pinRight)) == 0 ? true : false
+            } while (rightSensor)
+            basic.clearScreen()
+            direction = "0"
+        }
+
+        if (!rightSensor && !leftSensor && direction == "s") {
+            basic.showIcon(IconNames.Duck, 0)
+            rightSpeed = 115
+            leftSpeed = -115
+            PCAmotor.MotorRun(motor1, -leftSpeed)
+            PCAmotor.MotorRun(motor4, -rightSpeed)
+            basic.pause(600)
+            basic.clearScreen()
+            direction = "0"
+        }
+
+
+
+        if (rightSensor) {
+            rightSpeed = 115
+        } else {
+            rightSpeed = 0
+        }
+
+        if (leftSensor) {
+            leftSpeed = -115
+        } else {
+            leftSpeed = 0
+        }
+
+        PCAmotor.MotorRun(motor1, leftSpeed)
+        PCAmotor.MotorRun(motor4, -rightSpeed)
+    }
+    radio.onReceivedString(function (receivedString) {
+        if (receivedString == "s") {
+            direction = "s"
+        }
+        if (receivedString == "l") {
+            direction = "l"
+        }
+        if (receivedString == "r") {
+            direction = "r"
+        }
+    })
+})
+
+//OVLADAČ
+
+// radio.setGroup(69)
+// input.onButtonPressed(Button.A, function () {
+//     radio.sendString("l")
+//
+//     basic.showArrow(ArrowNames.West)
+//
+// })
+// input.onButtonPressed(Button.B, function () {
+//     radio.sendString("r")
+//
+//     basic.showArrow(ArrowNames.East)
+//
+// })
+// input.onButtonPressed(Button.AB, function () {
+//     radio.sendString("s")
+//
+//     basic.showArrow(ArrowNames.North)
+//
+// })
